ece4bef22ae6f302e879d53af8da50179d1123f7
King-Kingah <kingajames2015@gmail.com>
2021-05-19 15:48:52 +0000

    Add: mandatory square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is the "Square" module.
+
+This Square module provides a simple Square class.
+"""
+
+class Square:
+    """A simple Square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the "Square" module.
+
+This module provides a simple Square class with instantialize size.
+"""
+
+class Square:
+    """A classthat defines a Square by size"""
+    def __inti__(self, size):
+        self.__size = size
