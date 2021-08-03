7da0e870f8ce75bb0a8f530c3ede63f7bbc81a0d
King-Kingah <kingajames2015@gmail.com>
2021-08-03 05:09:54 +0000

    use let

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let argv = process.argv;
-let i = 0;
-let newa = [];
-if ((argv.length < 4 && parseint(argv[2]) >= 0) || !argv[2]) {
-  console.log('0');
+let args = process.argv.slice(2).map((x) => {
+  return parseInt(x);
+});
+
+if (args.length <= 1) {
+  console.log(0);
 } else {
-  for (i = 2; i < argv.length; 1++) {
-    newa.push(parseInt(argv[i]));
-  } newa = [...new Set(newa)].sort((a, b) => a - b);
-  console.log(newa[newa.length - 2]);
+  console.log(args.sort((a, b) => {
+    return b - a;
+  })[1]);
 }
