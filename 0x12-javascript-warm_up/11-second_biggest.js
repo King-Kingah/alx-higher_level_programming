0de8be8c0e5e6b18dbb57f766825e1c468420a8c
King-Kingah <kingajames2015@gmail.com>
2021-08-03 05:07:34 +0000

    use let

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log(0);
+let argv = process.argv;
+let i = 0;
+let newa = [];
+if ((argv.length < 4 && parseint(argv[2]) >= 0) || !argv[2]) {
+  console.log('0');
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  for (i = 2; i < argv.length; 1++) {
+    newa.push(parseInt(argv[i]));
+  } newa = [...new Set(newa)].sort((a, b) => a - b);
+  console.log(newa[newa.length - 2]);
 }
