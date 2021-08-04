e2298f8283258d9a093086bae626581b89313f60
King-Kingah <kingajames2015@gmail.com>
2021-08-04 11:58:05 +0000

    Added constructor

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-module.exports = {
-    Rectangle: function (w, h) {
+module.exports = class Rectangle {
+    constructor (w, h) {
       this.width = w;
       this.height = h;
     }
